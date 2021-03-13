2efba1cd75ee5cee25b10a00e899427cbb882968
EC2 Default User <ec2-user@ip-172-31-35-136.us-east-2.compute.internal>
2021-03-13 06:05:20 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+$('.slick').slick({
+    arrows:true,
+    prevArrow: '<button class="slide-arrow prev-arrow"><</button>',
+    nextArrow: '<button class="slide-arrow next-arrow">></button>',
+    autoplay:true,
+    autoplaySpeed: 3000, 
+});
