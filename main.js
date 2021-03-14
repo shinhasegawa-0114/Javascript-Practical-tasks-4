79cd648267feaf2b7c613f81c01d52b808193362
EC2 Default User <ec2-user@ip-172-31-35-136.us-east-2.compute.internal>
2021-03-14 14:34:20 +0000

    slick用CDNのURLの変更

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-$('.slick').slick({
+
+$(function(){
+  $('.slick').slick({
   arrows:true,
   prevArrow: '<button class="slide-arrow prev-arrow"><</button>',
   nextArrow: '<button class="slide-arrow next-arrow">></button>',
   autoplay:true,
   autoplaySpeed: 3000, 
+  });
 });
